997c085c7275c441a9aa1bfad6acf8569c2fa33b
somay007 <136879645+somay007@users.noreply.github.com>
2025-04-05 15:43:26 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,6 +25,7 @@ resource "azurerm_windows_web_app" "serviceApp" {
   name = var.linux_web_app_name
   resource_group_name = azurerm_resource_group.rgasp.name
   site_config {
+    always_on = false
     application_stack {
       node_version = "~22"
     }
