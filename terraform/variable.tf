1e6d4e7a9eb66fb1fe4f3bf2af38b1956fc74e77
somay007 <136879645+somay007@users.noreply.github.com>
2025-04-07 09:51:29 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -7,7 +7,7 @@ variable "subscription_id" {
 variable "location" {
     description = "location of service"
     type = string
-    default = "southafrica"
+    default = "southafricanorth"
 }
 variable "resource_group_name" {
   description = "resource group name"
